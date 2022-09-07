c5cb9bf9e7de4a4161fe90f810664b542c903696
unknown <rahulrai2822march@gmail.com>
2022-09-07 15:32:44 +0530

    first-commit

---
+++ basics.js
@@ -0,0 +1,31 @@
+// Datatype checking
+//console.log(typeof("hello"))
+
+// challenges
+//console.log(10+"20") // concatination
+//console.log(typeof(10+"20"))
+ // string + number = string
+ // string + string = string
+
+// console.log("9"-4) // returns //BUG
+//console.log(typeof(9-"4")) // NUMBER
+
+//console.log(typeof("" + ""))
+// console.log("hello" - "olaa") // NaN (NOT A NUMBER)
+// console.log(typeof("a"-"B")) // Number
+
+//console.log(true + true) // 1 + 1 = 2
+//console.log(typeof(true + true)) // NUMBER
+// boolean + boolean , boolean - boolean  = number 
+
+// var iAmUseless = null
+// console.log(iAmUseless) 
+// console.log(typeof(iAmUseless)) // returns object which and hence it is BUG
+
+// NaN Practice
+
+// console.log(NaN == NaN) // false
+// console.log(Number.NaN == NaN) //false
+// console.log(isNan(Nan)) //true
+// console.log(isNan(Number.NaN)) // true
+// console.log(Number.isNaN(NaN)) // true
